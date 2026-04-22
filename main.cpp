7848dbfb30a0b0ae27c36e312a1c0d2257605030
GLandier <gabinlandier@gmail.com>
2026-04-22 16:47:17 +0200

    debut

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <vector>
 #include "main.h"
 using namespace std;
+
 
 int main()
 {
     // Game initialization
-    game.initialize(10, Level::EASY_1, Mode::DEBUG, false, "Pseudo");
+    game.initialize(1, Level::EASY_1, Mode::DEBUG, false, "Pseudo");
+
+    vector<int> ligne;
+    vector<vector<int>> grille;
+
+    for (int i=0; i<3; i++){
+        ligne.push_back(0);
+    }
+
+    for (int i=0; i<3; i++){
+        grille.push_back(ligne);
+    }
 
     while (!game.isAllGameFinish())
     {
-        GameMove myMove{0, 0};
+        GameMove myMove{1,1};
 
         while (!game.isFinish())
         {
@@ -17,10 +30,18 @@ int main()
             GameMove gameMove;
             game.getMove(gameMove);
             std::cerr << "IA move " << gameMove.row << " " << gameMove.col << std::endl;
+            grille[gameMove.row][gameMove.col] = -1;
 
             // Send your move
             std::cerr << "Send move " << myMove.row << " " << myMove.col << std::endl;
             game.setMove(myMove);
+            grille[gameMove.row][gameMove.col] = 1;
+        }
+        for (vector<int> i:grille){
+            for (int j:ligne){
+                cout << j << " ";
+            }
+            cout << endl;
         }
     }
 
