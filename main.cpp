e6ed0c7ba4b7f5b9162eff43ad233e48039d26e8
GLandier <gabinlandier@gmail.com>
2026-04-23 15:02:49 +0200

    ajout du système de plateau

--- main.cpp
+++ main.cpp
@@ -3,22 +3,23 @@
 #include "main.h"
 using namespace std;
 
+bool estPlein(vector<vector<int>>& plateau, int debutL, int debutC) {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (plateau[debutL+i][debutC+j] == 0)
+                cout << "plein" << endl;
+                return false;
+    return true;
+}
 
 int main()
 {
     // Game initialization
     game.initialize(1, Level::EASY_1, Mode::DEBUG, false, "Pseudo");
 
-    vector<int> ligne;
-    vector<vector<int>> grille;
+    int size = 9;
+    vector<vector<int>> plateau(size, vector<int>(size, 0));
 
-    for (int i=0; i<3; i++){
-        ligne.push_back(0);
-    }
-
-    for (int i=0; i<3; i++){
-        grille.push_back(ligne);
-    }
 
     while (!game.isAllGameFinish())
     {
@@ -27,22 +28,70 @@ int main()
         while (!game.isFinish())
         {
             // Get IA move
+
             GameMove gameMove;
             game.getMove(gameMove);
+
             std::cerr << "IA move " << gameMove.row << " " << gameMove.col << std::endl;
-            grille[gameMove.row][gameMove.col] = -1;
+            plateau[gameMove.row][gameMove.col] = -1;
+
+            estPlein(plateau,gameMove.row,gameMove.col);
+
+            if(plateau[1][1]==0){
+                myMove.row=1;
+                myMove.col=1;
+            }
+            else if(plateau[0][1] == 0){
+                myMove.row=0;
+                myMove.col=1;
+            }
+            else if(plateau[0][2]==0){
+                myMove.row=0;
+                myMove.col=2;
+            }
+            else if(plateau[2][1]==0){
+                myMove.row=2;
+                myMove.col=1;
+            }
+            else if(plateau[2][2]==0){
+                myMove.row=2;
+                myMove.col=2;
+            }
+            else if(plateau[0][0]==0){
+                myMove.row=0;
+                myMove.col=0;
+            }
+            else if(plateau[1][0]==0){
+                myMove.row=1;
+                myMove.col=0;
+            }
+            else if(plateau[1][1]==0){
+                myMove.row=1;
+                myMove.col=1;
+            }
+            else if(plateau[2][2]==0){
+                myMove.row=2;
+                myMove.col=2;
+            }
+            else if(plateau[1][2]==0){
+                myMove.row=1;
+                myMove.col=2;
+            }
 
             // Send your move
             std::cerr << "Send move " << myMove.row << " " << myMove.col << std::endl;
             game.setMove(myMove);
-            grille[gameMove.row][gameMove.col] = 1;
+            plateau[myMove.row][myMove.col] = 1;
+            estPlein(plateau,myMove.row,myMove.col);
         }
-        for (vector<int> i:grille){
-            for (int j:ligne){
-                cout << j << " ";
+
+        for (vector<int> grille:plateau){
+            for (int j:grille){
+                cout << j;
             }
-            cout << endl;
+            cout << " " << endl;
         }
+
     }
 
     return 0;
